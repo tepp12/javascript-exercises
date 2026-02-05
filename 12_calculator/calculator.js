37bfd691382082f8cb6dd3559e42f583dc1920fe
Guilherme Ribeiro <guilherme.rego@souunit.com.br>
2026-02-04 21:32:37 -0300

    Solve problem 12 utilizing incredible programming

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(theArray) {
+    return theArray.reduce((sum, item) => {
+    sum += item
+    return sum;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(num1, num2) {
+  return num1*num2
 };
 
-const power = function() {
-	
+const power = function(num1, exponent) {
+    return Math.pow(num1, exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(num1, factory) {
+    if (factory <= 1) return 1
+  for (let i=factory; i >= 1; i--){
+    num1 *= i
+  }
+  return num1
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let array = string.split("")
+    array = array.reverse()
+    let reversedString = array.join("")
+    string == reversedString ? true : false
 };
 
 // Do not edit below this line
