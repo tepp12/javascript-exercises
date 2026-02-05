0e9087774730b8074eaf9fc121cdf4c0fb314f31
Guilherme Ribeiro <guilherme.rego@souunit.com.br>
2026-02-04 22:12:08 -0300

    Solve problem 13

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 0){
+        return 0
+    }
+    if (num == 1){
+        return 1
+    }
+    if (num < 0){
+        return
+    }
+    let result = Math.round(Math.pow((1/2)*(1+(Math.sqrt(5))), num)/Math.sqrt(5))
+    return result
 };
 
 // Do not edit below this line
