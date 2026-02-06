e16cad8e9538db6306f6e7349afa64acdfc8eeb1
Guilherme Ribeiro <guilherme.rego@souunit.com.br>
2026-02-05 22:17:17 -0300

    so smart

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let sortedArray = array.sort((a, b) => {
+        if (getAge(a.yearOfBirth, a.yearOfDeath) > getAge(b.yearOfBirth, b.yearOfDeath)){
+            return 1
+        } if (getAge(a.yearOfBirth, a.yearOfDeath) == getAge(b.yearOfBirth, b.yearOfDeath)) {
+            return 0
+        } if (getAge(a.yearOfBirth, a.yearOfDeath) < getAge(b.yearOfBirth, b.yearOfDeath)) {
+            return -1
+        }
+    })
+    console.log(sortedArray)
+    return sortedArray.at(-1).name
+};
 
+const getAge = function(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
